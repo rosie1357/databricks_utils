d139fa9608d902e08389e2dca6ca964e95cda539
rosie.malsberger@definitivehc.com <rosie.malsberger@definitivehc.com>
2023-02-14 15:57:32 +0000

    feat: add ptiles func

--- _general_funcs/dhc_data_funcs.py
+++ _general_funcs/dhc_data_funcs.py
@@ -23,38 +23,75 @@ def get_primary_affiliation(addtl_cols=[]):
     
     
     return spark.sql(f"""
+    
+        select c.*
+               ,d.hq_city as defhc_city
+               ,d.hq_state as defhc_state
+               ,d.firm_type as defhc_type
+        
+        from (
+            select coalesce(a.physician_npi, b.npi) as physician_npi
+                   ,a.defhc_id
+                   ,a.defhc_name
+                   ,b.ProviderName
+                   ,b.primaryspecialty
+                   {cols_stmt}
 
-        select a.*
-               ,b.hq_city as defhc_city
-               ,b.hq_state as defhc_state
-               ,b.firm_type as defhc_type
-               ,c.primaryspecialty
-               {cols_stmt}
+           from (
 
-       from (
+                select physician_npi
+                       ,defhc_id
+                       ,hospital_name as defhc_name
+                       ,row_number() over (partition by physician_npi 
+                                           order by score_bucket desc, score desc)
+                                     as rn
 
-            select physician_npi
-                   ,defhc_id
-                   ,hospital_name as defhc_name
-                   ,row_number() over (partition by physician_npi 
-                                       order by score_bucket desc, score desc)
-                                 as rn
+                        from   hcp_affiliations.physician_org_affiliations
+                        where  include_flag = 1 and
+                               current_flag = 1
 
-                    from   hcp_affiliations.physician_org_affiliations
-                    where  include_flag = 1 and
-                           current_flag = 1
+                   ) a
 
-               ) a
+           full join
+           martdim.d_provider b
+           on a.physician_npi = b.npi
+           
+           where rn=1 or rn is null
+
+           ) c
 
        left join  
-       definitivehc.hospital_all_companies b
-       on a.defhc_id = b.hospital_id
-
-       left join
-       martdim.d_provider c
-       on a.physician_npi = c.npi
-
-       where rn=1
-           
+       definitivehc.hospital_all_companies d
+       on c.defhc_id = d.hospital_id
            
    """)
+
+# COMMAND ----------
+
+def create_ind(CodeType, ind, codes, string_values=True, getmax=False):
+    """
+    Function create_ind to return statement to create indicator based on code sets (based on using table mxmart.f_mxcodes)
+    params:
+        CodeType str: value for CodeType to condition on, eg 'DIAGNOSIS'
+        ind str: name of indicator to create if match, will add _code to the end
+        codes list: list of codes to match on
+        string_values bool: optional param to specify whether codes are string and to put quotes around each, default = True
+        getmax bool: optional param to specify whether the ind will be used in aggregations and should put max() around statement, default = False
+        
+    returns:
+        case statement to create ind
+    
+    """
+    
+    if string_values:
+        
+        codes_str = ', '.join(f"'{c}'" for c in codes)
+    else:
+        codes_str = ', '.join(f'{c}' for c in codes)
+        
+    stmt = f"case when upper(CodeType) = '{CodeType}' and CodeValue in ({codes_str}) then 1 else 0 end"
+    
+    if getmax:
+        stmt = f"max({stmt})"
+    
+    return f"{stmt} as {ind}_code"
--- _general_funcs/pdf_funcs.py
+++ _general_funcs/pdf_funcs.py
@@ -5,6 +5,10 @@
 
 # COMMAND ----------
 
+import math
+
+# COMMAND ----------
+
 def frequency(df, cols, drop_null=False):
     """
     Function to get freq of cols on input df and return df
@@ -55,4 +59,48 @@ def concatenate_cols(df, cols, sep=' '):
     """
     
     return pd.Series(df[cols].fillna('').values.tolist()).map(lambda c: sep.join(c))
+
+# COMMAND ----------
+
+def create_ptiles(df, col, ptiles, fillna=True, suffix='ptile', **kwargs):
+    """
+    Function create_ptiles to read in given df and create ptiles
+    params:
+        df: pandas df
+        ptiles int: # of buckets to create
+        col str: name of column to create ptiles from
+        fillna bool: optional param to specify whether to fill col with 0s if na, default = True
+        suffix str: optional param to specify suffix to add to col name for new ptile col, default = 'ptile'
+        **kwargs:
+            sort_cols list: optional param to list additional cols to sort by to deal with possible ties
+            outcol str: optional param to specify name of output column, otherwise = {col}_{suffix}
+        
+    returns:
+        df with cumpct col and ptiles col
     
+    """
+    
+    # create additional vars
+    
+    sort_cols = [col] + kwargs.get('sort_cols', [])
+    
+    outcol = kwargs.get('outcol', f"{col}_{suffix}")
+    
+    div = 100/ptiles
+    
+    # fill with 0s if requested
+    
+    if fillna:
+        df[col].fillna(0, inplace=True)
+        
+    # sort and create % and ptile - if sum total of column = 0, set to 1 to avoid nan error (all ptiles will == 1 as dummy values)
+    
+    df_sorted = df.sort_values(sort_cols)[col]
+    
+    tot = max(df_sorted.sum(), 1)
+    
+    df[f"{col}_cumpct"] = 100*(df_sorted.cumsum() / tot)
+    
+    df[outcol] = df[f"{col}_cumpct"].apply(lambda x: min(1 + math.ceil(x//div), ptiles))
+    
+    return df

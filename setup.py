e7b4b6bc51e108a973036fb37faf184760cbc588
Rosie Malsberger <rosie.malsberger@definitivehc.com>
2023-04-07 15:23:03 -0400

    build: add setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+This file configures the Python package with entrypoints used for future runs on Databricks
+
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="general_funcs",
+    packages=find_packages("src"), 
+    package_dir={"": "src"},
+    setup_requires=["wheel"],
+    description="",
+    author="",
+)

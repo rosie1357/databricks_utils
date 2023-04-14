39e8089362170ab1a61fe0bab020beb34130594c
Rosie Malsberger <rosie.malsberger@definitivehc.com>
2023-04-14 15:07:20 -0400

    fix: add spark context to print comp funcs

--- src/_general_funcs/sdf_print_comp_funcs.py
+++ src/_general_funcs/sdf_print_comp_funcs.py
@@ -1,5 +1,8 @@
 import datacompy
 from pyspark.sql import functions as F
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
 
 def sdf_check_distinct(sdf, cols):
     """

b9ce9b3adfc73fc5522f4453567051b1fd573d8c
Rosie Malsberger <rosie.malsberger@definitivehc.com>
2023-04-21 11:35:45 -0400

    fix: fix timeit to return function returns

--- src/_general_funcs/decorators.py
+++ src/_general_funcs/decorators.py
@@ -1,17 +1,17 @@
-import time
+from functools import wraps
 
-def timeit(func):
-    """
-    timeit() decorator to print total elapsed time after run of given func,
-        optional kwargs = 'time' with a default of minutes, other options = 'seconds'
-    
-    """
-    def wrapper_function(*args, **kwargs):
-        start = time.perf_counter()
-        func(*args,  **kwargs)
-        time_seconds = time.perf_counter()-start
-        if kwargs.get('time','')=='seconds':
-            print('\t' + f"Total time: {int(time_seconds)} seconds")
-        else:
-            print('\t' + f"Total time: {int(time_seconds//60)} minutes")
-    return wrapper_function
+def timeit(**dec_kwargs):
+    time_param = dec_kwargs.get('time','')
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.perf_counter()
+            returns = func(*args,  **kwargs)
+            time_seconds = time.perf_counter()-start
+            if time_param=='seconds':
+                print('\t' + f"Total time: {int(time_seconds)} seconds")
+            else:
+                print('\t' + f"Total time: {int(time_seconds//60)} minutes")
+            return returns
+        return wrapper
+    return decorator
